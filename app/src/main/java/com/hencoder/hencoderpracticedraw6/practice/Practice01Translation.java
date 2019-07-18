2dba6d40848d48559c35dcc3062a2103bb4f8cc1
范凯 <fankai@fankaideMacBook-Pro.local>
2019-07-18 23:05:39 +0800

    01

--- app/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice01Translation.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice01Translation.java
@@ -21,6 +21,8 @@ import static com.hencoder.hencoderpracticedraw6.Utils.dpToPixel;
 public class Practice01Translation extends RelativeLayout {
     Button animateBt;
     ImageView imageView;
+    int translationState = 0;
+    int translationCount = SDK_INT > Build.VERSION_CODES.LOLLIPOP ? 4 : 3;
 
     public Practice01Translation(Context context) {
         super(context);
@@ -40,15 +42,40 @@ public class Practice01Translation extends RelativeLayout {
 
         animateBt = (Button) findViewById(R.id.animateBt);
         imageView = (ImageView) findViewById(R.id.imageView);
-        if (SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
-            // 给音乐图标加上合适的阴影
-            imageView.setOutlineProvider(new MusicOutlineProvider());
-        }
+//        if (SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+//            // 给音乐图标加上合适的阴影
+//            imageView.setOutlineProvider(new MusicOutlineProvider());
+//        }
 
         animateBt.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(final View v) {
                 // TODO 在这里处理点击事件，通过 View.animate().translationX/Y/Z() 来让 View 平移
+                /**
+                 * 其中带有 -By() 后缀的是增量版本的方法，
+                 * 例如，translationX(100) 表示用动画把 View 的 translationX 值渐变为 100，
+                 * 而  translationXBy(100) 则表示用动画把 View 的 translationX 值渐变地增加 100。
+                 * */
+                switch (translationState) {
+                    case 0:
+                        imageView.animate().translationXBy(dpToPixel(100));
+                        break;
+                    case 1:
+                        imageView.animate().translationXBy(-dpToPixel(100));
+                        break;
+                    case 2:
+                        imageView.animate().translationYBy(dpToPixel(50));
+                        break;
+                    case 3:
+                        imageView.animate().translationYBy(-dpToPixel(50));
+                        break;
+
+                }
+                translationState++;
+                if (translationState == translationCount) {
+                    translationState = 0;
+                }
+
             }
         });
     }
